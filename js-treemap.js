726f95c0f4308bb1db3a61a7480c551b45dc3acb
Alexander During <alexanderduring.git@gmail.com>
2016-02-17 17:15:51 +0100

    Added second implementation for layoutRow to fix border issue (Work in progress).

--- js-treemap.js
+++ js-treemap.js
@@ -3,13 +3,13 @@ function Treemap(context)
 {
     var overallSize = {width:0, height:0}
     var currentOffset = {x:0, y:0};
-    var currentSize = {width:0, height:0}
+    var currentSize = {width:0, height:0, area:0}
     var rectangles = [];
     var colors = [];
 
 
 
-    function worstRatio(areas, width)
+    function worstRatio(areas, length)
     {
         var worstRatio;
 
@@ -20,8 +20,8 @@ function Treemap(context)
             var areaMax = Math.max.apply(null, areas);
             var areaSum = listSum(areas);
             
-            var ratioOne = (width*width*areaMax) / (areaSum*areaSum);
-            var ratioTwo = (areaSum*areaSum) / (width*width*areaMin);
+            var ratioOne = (length*length*areaMax) / (areaSum*areaSum);
+            var ratioTwo = (areaSum*areaSum) / (length*length*areaMin);
             
             worstRatio = Math.max.apply(null, [ratioOne, ratioTwo]);
         }
@@ -31,21 +31,22 @@ function Treemap(context)
 
 
 
-    function squarify(children, row, width)
+    function squarify(children, row)
     {
         if (children.length > 0) {
+            var length = lengthOfShortestSide();
             var head = children.slice(0, 1);
             var tail = children.slice(1);
             var rowWithChild = row.concat(head);
 
-            if (worstRatio(row, width) >= worstRatio(rowWithChild, width)) {
-                squarify(tail, rowWithChild, width);
+            if (worstRatio(row, length) >= worstRatio(rowWithChild, length)) {
+                squarify(tail, rowWithChild);
             } else {
-                layoutRow(row);
-                squarify(children, [], lengthOfShortestSide());
+                layoutRow2(row);
+                squarify(children, []);
             }
         } else {
-            layoutRow(row);
+            layoutRow2(row);
         }
     }
 
@@ -102,16 +103,21 @@ function Treemap(context)
         var tempOffset;
 
         if (shortestSide() == 'height') {
-            var layoutWidth = sum / lengthOfShortestSide();
+            var layoutWidth = Math.round(sum / lengthOfShortestSide());
+//            console.log('Shortest side is height: layoutWidth = '+sum+' / '+lengthOfShortestSide()+' = '+layoutWidth);
             tempOffset = currentOffset.y;
 
             row.forEach(function(element) {
-                elementHeight = element / layoutWidth;
+                elementHeight = Math.round(element / layoutWidth);
 
                 var x1 = currentOffset.x;
                 var y1 = tempOffset;
                 var x2 = currentOffset.x + layoutWidth;
                 var y2 = tempOffset + elementHeight;
+
+                // Fix rounding errors
+                x2 = getSaveX(x2);
+                y2 = getSaveY(y2);
 
                 rectangles[rectangles.length] = {'x1': x1, 'y1': y1, 'x2': x2, 'y2': y2};
                 tempOffset = tempOffset + elementHeight;
@@ -119,18 +125,24 @@ function Treemap(context)
 
             currentOffset.x = currentOffset.x + layoutWidth;
             currentSize.width = currentSize.width - layoutWidth;
+            currentSize.area = currentSize.width * currentSize.height;
 
         } else {
-            var layoutHeight = sum / lengthOfShortestSide();
+            var layoutHeight = Math.round(sum / lengthOfShortestSide());
+//            console.log('Shortest side is width: layoutHeight = '+sum+' / '+lengthOfShortestSide()+' = '+layoutHeight);
             tempOffset = currentOffset.x;
 
             row.forEach(function(element) {
-                elementWidth = element / layoutHeight;
+                elementWidth = Math.round(element / layoutHeight);
 
                 var x1 = tempOffset;
                 var y1 = currentOffset.y;
                 var x2 = tempOffset + elementWidth;
                 var y2 = currentOffset.y + layoutHeight;
+
+                // Fix rounding errors
+                x2 = getSaveX(x2);
+                y2 = getSaveY(y2);
 
                 rectangles[rectangles.length] = {x1: x1, y1: y1, x2: x2, y2: y2};
                 tempOffset = tempOffset + elementWidth;
@@ -138,6 +150,80 @@ function Treemap(context)
 
             currentOffset.y = currentOffset.y + layoutHeight;
             currentSize.height = currentSize.height - layoutHeight;
+            currentSize.area = currentSize.width * currentSize.height;
+        }
+
+    }
+
+
+
+    function layoutRow2(row)
+    {
+        var tempOffset;
+
+        if (shortestSide() == 'height') {
+
+            // Calculate row width
+            var rowArea = sumRow(row);
+            var rowAreaPercentage = rowArea / currentSize.area;
+            console.log('rowAreaPercentage:'+rowAreaPercentage);
+            var layoutWidth = Math.round(currentSize.width * rowAreaPercentage);
+            var tempHeight = lengthOfShortestSide();
+            tempOffset = currentOffset.y;
+
+            while (row.length > 0) {
+
+                // Calculate element
+                var elementArea = row[0];
+                var elementAreaPercentage = elementArea / sumRow(row);
+                var elementHeight = Math.round(tempHeight * elementAreaPercentage);
+                var x1 = currentOffset.x;
+                var y1 = tempOffset;
+                var x2 = x1 + layoutWidth - 1;
+                var y2 = y1 + elementHeight - 1;
+                rectangles[rectangles.length] = {'x1': x1, 'y1': y1, 'x2': x2, 'y2': y2};
+
+                // Prepare next iteration
+                tempHeight = tempHeight - elementHeight;
+                tempOffset = tempOffset + elementHeight;
+                row.shift();
+            }
+
+            currentOffset.x = currentOffset.x + layoutWidth;
+            currentSize.width = currentSize.width - layoutWidth;
+            currentSize.area = currentSize.width * currentSize.height;
+
+        } else {
+
+            // Calculate row width
+            var rowArea = sumRow(row);
+            var rowAreaPercentage = rowArea / currentSize.area;
+            console.log('rowAreaPercentage:'+rowAreaPercentage);
+            var layoutHeight = Math.round(currentSize.height * rowAreaPercentage);
+            var tempWidth = lengthOfShortestSide();
+            tempOffset = currentOffset.x;
+
+            while (row.length > 0) {
+
+                // Calculate element
+                var elementArea = row[0];
+                var elementAreaPercentage = elementArea / sumRow(row);
+                var elementWidth = Math.round(tempWidth * elementAreaPercentage);
+                var x1 = tempOffset;
+                var y1 = currentOffset.y;
+                var x2 = x1 + elementWidth - 1;
+                var y2 = y1 + layoutHeight - 1;
+                rectangles[rectangles.length] = {x1: x1, y1: y1, x2: x2, y2: y2};
+
+                // Prepare next iteration
+                tempWidth = tempWidth - elementWidth;
+                tempOffset = tempOffset + elementWidth;
+                row.shift();
+            }
+
+            currentOffset.y = currentOffset.y + layoutHeight;
+            currentSize.height = currentSize.height - layoutHeight;
+            currentSize.area = currentSize.width * currentSize.height;
         }
 
     }
@@ -163,7 +249,10 @@ function Treemap(context)
 
 
 
-
+    /**
+     * Takes a list of values and puts these value in relation to the total sum of the values.
+     * Then it returns a new list with values that have the same relation to the total area of the map
+     */
     function normalize(list)
     {
         var normalizedList = [];
@@ -184,6 +273,7 @@ function Treemap(context)
     {
         normalizedList = normalize(list);
         squarify(normalizedList, [], lengthOfShortestSide());
+        console.log(rectangles);
     }
 
 
@@ -202,16 +292,23 @@ function Treemap(context)
         var height = rectangle.y2-rectangle.y1;
         var hue = colors.shift();
 
+        // Draw rectangle
         context.beginPath();
-        context.rect(rectangle.x1, rectangle.y1, width, height);
+        context.rect(rectangle.x1+1, rectangle.y1+1, width, height);
         context.fillStyle = 'hsl('+hue+', 50%, 80%)';
         context.fill();
         context.lineWidth = 2;
         context.strokeStyle = 'white';
         context.stroke();
 
+        // Draw text
+        context.font='10px Verdana';
+        context.fillStyle = '#000000';
+        context.fillText('x:'+(rectangle.x1+1)+',y:'+(rectangle.y1+1), rectangle.x1+3, rectangle.y1+15);
+        context.fillText('w:'+width+',h:'+height, rectangle.x1+3, rectangle.y1+30);
+
         if (rectangles.length > 0) {
-            setTimeout(function() {drawRectangle()}, 30);
+            setTimeout(function() {drawRectangle()}, 300);
         }
     }
 
@@ -248,21 +345,25 @@ function Treemap(context)
 
     this.draw = function(data)
     {
+        var width = context.canvas.width-2;
+        var height = context.canvas.height-2;
+
         rectangles = [];
 
         overallSize = {
-            width: context.canvas.width-2,
-            height: context.canvas.height-2
+            width: width,
+            height: height
         };
 
         currentSize = {
-            width: context.canvas.width-2,
-            height: context.canvas.height-2
+            width: width,
+            height: height,
+            area: width * height
         };
 
         currentOffset = {
-            x: 1,
-            y: 1
+            x: 0,
+            y: 0
         };
 
         sortDescending(data);
